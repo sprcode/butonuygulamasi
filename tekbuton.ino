513360d116c659c8ba35b6437fd540348e519d43
sprcode <47007407+sprcode@users.noreply.github.com>
2019-01-29 12:55:33 +0300

    Add files via upload

---
+++ tekbuton.ino
@@ -0,0 +1,14 @@
+int led=10;
+int buton=5;
+void setup() {
+pinMode(led,OUTPUT);
+pinMode(buton,INPUT);
+}
+void loop() {
+if (digitalRead(buton)==1){
+digitalWrite(led,HIGH);
+}
+else{
+digitalWrite(led,LOW);
+}
+}
